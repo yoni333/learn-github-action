2303fb2e32d1c646981952b525804c54b8efcc8f
yehonatan yehezkel <yoni333@gmail.com>
2023-10-30 10:53:30 +0200

    fix res undefined

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 console.log("run test here!!!!!!****")
 // const axios = require("axios");
 const axios = require('axios');
-async function testHTTP (){
-const res = await axios.get("https://5f1455762710570016b37ea7.mockapi.io/users",{timeout: 3000}).then(function (response) {
-    // handle success
-    console.log('success');
-    console.log(response);
-})
-    .catch(function (error) {
-        // handle error
-        console.log("error");
-        console.log(error);
+async function testHTTP() {
+    const res = await axios.get("https://5f1455762710570016b37ea7.mockapi.io/users", { timeout: 3000 }).then(function (response) {
+        // handle success
+        console.log('success');
+        console.log(response);
+        return response
     })
-    .finally(function () {
-        // always executed
-    });
+        .catch(function (error) {
+            // handle error
+            console.log("error");
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
 
-    console.log('res',res.status);
+    console.log('res', res.status);
 
 }
 
