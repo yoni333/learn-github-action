80a505d6c76596f8d9a223da5c38a55659b70953
yehonatan yehezkel <yoni333@gmail.com>
2023-10-30 09:13:26 +0200

    chack axios

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 console.log("run test here!!!!!!****")
+// const axios = require("axios");
+const axios = require('axios/dist/node/axios.cjs');
+axios.get("https://5f1455762710570016b37ea7.mockapi.io/users").then(function (response) {
+    // handle success
+    console.log('success');
+    console.log(response);
+})
+    .catch(function (error) {
+        // handle error
+        console.log("error");
+        console.log(error);
+    })
+    .finally(function () {
+        // always executed
+    });
