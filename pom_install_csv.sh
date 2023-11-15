64b1d4211612de563f1fecb186c2c9e8a868cf49
yehonatan yehezkel <yoni333@gmail.com>
2023-11-15 13:36:09 +0200

    j 22

---
+++ pom_install_csv.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Path to the CSV file
+CSV_FILE="pom-update.csv"
+
+# Check if CSV file exists
+if [ ! -f "$CSV_FILE" ]; then
+    echo "CSV file not found!"
+    exit 1
+fi
+
+# Extract the first column and save it into a variable
+# Use awk to extract unique values from the first column
+FOLDERS=$(awk -F, '!seen[$1]++ {print $1}' "$CSV_FILE")
+
+# Print the unique folder names
+echo "$FOLDERS"
+
+# Jenkins provides a WORKSPACE environment variable
+WORKSPACE_PATH=$WORKSPACE
+
+# Loop through each folder
+for folder in $FOLDERS; do
+    # Skip if folder is empty
+    if [ -z "$folder" ]; then
+        continue
+    fi
+
+    echo "Changing to directory: $folder"
+    # Change to the directory
+    cd "$folder"
+
+    # Check if directory change was successful
+    if [ $? -eq 0 ]; then
+        echo "Running meaven install in $folder"
+        # Run meaven install
+        mvn -B clean install -DskipTests -Dspring-boot.repackage.skip=true
+        
+         mvn dependency:copy-dependencies -DoutputDirectory="$WORKSPACE_PATH/$FOLDER_NAME/dependencies"
+
+        # Check if meaven install was successful
+        if [ $? -ne 0 ]; then
+            echo "meaven install failed in $folder"
+            exit 1
+        fi
+
+        
+        # Zip the folder with its updated package.json and the npm-packages directory
+        echo "Zipping the folder $folder"
+        
+        # Define the folder name and store it in a variable
+        zip_folder="zip_for_halbana"
+
+        # Check if the folder exists; if not, create it
+        if [ ! -d "$zip_folder" ]; then
+            mkdir "../$zip_folder"
+        fi
+        # zip the npm-packages folder
+        tar -czvf "${folder}_pom-update.tgz"  dependencies/ pom-update.csv
+        # Move files to the specified folder
+        echo "move the tgz file to root folder"
+        mv "${folder}_pom-update.tgz" "../$zip_folder/"
+
+   
+    else
+        echo "Directory $folder not found!"
+    fi
+
+    # Return to the original directory (Jenkins workspace)
+    cd -
+done
+
+echo "Script execution completed!"
